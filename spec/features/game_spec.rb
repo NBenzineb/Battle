974fa23f6c0e75afb76986bb7cce87bc15606bd2
Michael Su <mailmsuu@gmail.com>
2022-04-06 16:32:15 +0100

    tried to use doubles (but don't think they are being used)

--- spec/features/game_spec.rb
+++ spec/features/game_spec.rb
@@ -1,15 +1,21 @@
 require './app.rb'
 
-feature "Attack" do
-  scenario "can attack player 2" do
-    sign_in_and_play
-    click_on('attack')
-    expect(page).to have_content("You have attacked player 2")
-  end
+describe Game do
+  let(:game) { described_class.new }
+  let(:player1) { double "player1" }
+  let(:player2) { double "player1" }
 
-  scenario "attack- reduces hp by 10" do
-    sign_in_and_play
-    click_on('attack')
-    expect(page).to have_content(90)
+  feature "Attack" do
+    scenario "can attack player 2" do
+      sign_in_and_play
+      click_on('attack')
+      expect(page).to have_content("You have attacked player 2")
+    end
+
+    scenario "attack- reduces hp by 10" do
+      sign_in_and_play
+      click_on('attack')
+      expect(page).to have_content(90)
+    end
   end
 end
