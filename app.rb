feaffcd1e5aaff92addd4c806c95e7158f9c2bb1
Michael Su <mailmsuu@gmail.com>
2022-04-06 14:49:36 +0100

    fixed chap20, now redirects and saves variable names properly

--- app.rb
+++ app.rb
@@ -16,19 +16,14 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    p params
     session[:player1] = params[:player1]
     session[:player2] = params[:player2]
-    p session[:player1]
-    p session[:player2]
     redirect '/play'
   end
 
   get '/play' do
-    p session[:player1]
     @player1 = session[:player1]
     @player2 = session[:player2]
-    
     erb(:play)
   end
 
